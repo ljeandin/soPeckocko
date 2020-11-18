7e8ac06f536d00610a510852130d48d1bd12f842
ljeandin <luciejeandin@gmail.com>
2020-11-18 10:14:04 +0100

    comments added

--- backend/routes/Sauce.js
+++ backend/routes/Sauce.js
@@ -5,7 +5,7 @@ const sauceCtrl = require('../controllers/Sauce'); //controller location
 const auth = require('../middleware/auth'); //authentication middleware
 const multer = require('../middleware/multer-config'); //multer-config middleware
 
-router.get('', auth, sauceCtrl.getAllSauces);
+router.get('', auth, sauceCtrl.getAllSauces); //router.httpVerb('URL', middleware(s), path to the right controller)
 router.post('', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
